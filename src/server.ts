9f8b3e704858897ed23110a444e2ad661503e65d
Nadia <nlnadialigia@gmail.com>
2021-02-22 21:22:29 -0300

    feat: add get and post routes examples

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World - NLW04' });
+});
+
+app.post('/', (request, response) => {
+  return response.json({ message: 'Os dados foram salvos com sucesso' });
+});
+
 app.listen(3080, () => {
   console.log('Server is running!');
 });

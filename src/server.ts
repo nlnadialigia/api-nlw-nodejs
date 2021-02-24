f36a5635f9895f180f2013880144ec95ece18781
Nadia <nlnadialigia@gmail.com>
2021-02-23 22:59:39 -0300

    feat: add UserController

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
 import express from 'express';
 import 'reflect-metadata';
 import './database';
+import { router } from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World - NLW04' });
-});
-
-app.post('/', (request, response) => {
-  return response.json({ message: 'Os dados foram salvos com sucesso' });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3080, () => {
   console.log('Server is running!');
